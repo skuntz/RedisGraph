d0de66763d16491f28b5f2158293b90584a1489b
Jason Riedy <jriedy@emutechnology.com>
2020-10-17 16:20:55 -0400

    Implement iterators generically outside of the GraphBLAS.
    Purpose: Permit easier use of other GraphBLAS implementations, including
    more recent SuiteSparse GraphBLAS, system-installed GraphBLAS libraries,
    and libraries using external accelerators.
    
    The general method is to extract a row vector at a time from the matrix,
    recover its pattern, and iterate over the pattern.
    
    Caveats:  Column vectors are treated as rows to match RedisGraph
    expectations.
    
    Also in test_tuples_iter, test against an out-of-bounds error when out of bounds.

--- src/GraphBLASExt/GxB_Matrix_tuple_iter.h
+++ src/GraphBLASExt/GxB_Matrix_tuple_iter.h
@@ -1,5 +1,3 @@
-#if !defined(GxB_Matrix_tuple_iter_H_)
-#define GxB_Matrix_tuple_iter_H_
 //------------------------------------------------------------------------------
 // GxB_MatrixTupleIter:  Iterates over all none zero values of a matrix
 //------------------------------------------------------------------------------
@@ -9,30 +7,25 @@ typedef struct GBr_MatrixTupleIter_opaque* GxB_MatrixTupleIter;
 // Create a new matrix iterator
 GrB_Info GxB_MatrixTupleIter_new
 (
-    GxB_MatrixTupleIter *gxb_iter, // iterator to create
+    GxB_MatrixTupleIter *iter, // iterator to create
     GrB_Matrix A                // matrix being iterated
-) ;
-
-// Iterate over specific row
-GrB_Info GxB_MatrixTupleIter_iterate_row
-(
-    GxB_MatrixTupleIter gxb_iter,  // iterator to use
-    GrB_Index rowIdx            // row index to iterate over
 ) ;
 
 // Move iterator over specific rows range
 GrB_Info GxB_MatrixTupleIter_iterate_range
 (
-    GxB_MatrixTupleIter gxb_iter,  // iterator to use
+    GxB_MatrixTupleIter iter,  // iterator to use
     GrB_Index startRowIdx,      // row index to start with
     GrB_Index endRowIdx         // row index to finish with
 ) ;
 
+GrB_Info 
+GxB_MatrixTupleIter_iterate_row (GxB_MatrixTupleIter iter, GrB_Index rowIdx);
 
 // Advance iterator to the next none zero value
 GrB_Info GxB_MatrixTupleIter_next
 (
-    GxB_MatrixTupleIter gxb_iter,      // iterator to consume
+    GxB_MatrixTupleIter iter,      // iterator to consume
     GrB_Index *row,                 // optional row index of current NNZ
     GrB_Index *col,                 // optional column index of current NNZ
     bool *depleted                  // indicate if iterator depleted
@@ -41,19 +34,18 @@ GrB_Info GxB_MatrixTupleIter_next
 // Reset iterator
 GrB_Info GxB_MatrixTupleIter_reset
 (
-    GxB_MatrixTupleIter gxb_iter        // iterator to reset
+    GxB_MatrixTupleIter iter        // iterator to reset
 ) ;
 
 // Reuse iterator to scan given matrix
 GrB_Info GxB_MatrixTupleIter_reuse
 (
-    GxB_MatrixTupleIter gxb_iter,       // iterator to update
+    GxB_MatrixTupleIter iter,       // iterator to update
     GrB_Matrix A            // matrix to scan
 ) ;
 
 // Release every resource consumed by iterator
 GrB_Info GxB_MatrixTupleIter_free
 (
-    GxB_MatrixTupleIter *gxb_iter        // iterator to free
+    GxB_MatrixTupleIter *iter        // iterator to free
 ) ;
-#endif
